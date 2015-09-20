407337b7b64e97bd55ce254887e2d799be99c416
asdfhamiltonian <max.s.hirsh@gmail.com>
2015-09-19 19:09:29 -0700

    initial commit

---
+++ planets_api.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from planets import Planets
+
+app = Flask(__name__)
+api = Api(app)
+
+class RiseSet(Resource):
+    def get(self, latitude, longitude, timezone):
+        #flask can't handle negative number input so had to convert from strings
+        latitude = float(latitude)
+        longitude = float(longitude)
+        timezone = float(timezone)
+        location = Planets(latitude, longitude)
+        riseSetString = location.allRiseSet(-7)
+        return {'Rise/Set Times': riseSetString}
+
+class PositionNow(Resource):
+    def get(self, latitude, longitude):
+        #flask can't handle negative number input so had to convert from strings
+        latitude = float(latitude)
+        longitude = float(longitude)
+        location = Planets(latitude, longitude)
+        now = {}
+        now["Venus"] = location.calcVenusNow()
+        now["Mercury"] = location.calcMercuryNow()
+        now["Mars"] = location.calcMarsNow()
+        now["Jupiter"] = location.calcJupiterNow()
+        now["Saturn"] = location.calcSaturnNow()
+        now["Uranus"] = location.calcUranusNow()
+        now["Neptune"] = location.calcNeptuneNow()
+        now["Pluto"] = location.calcPlutoThatIsNotAPlanetNow()
+        return now
+
+api.add_resource(RiseSet, '/RiseSet/<string:latitude>/<string:longitude>/<string:timezone>')
+api.add_resource(PositionNow, '/PositionNow/<string:latitude>/<string:longitude>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
